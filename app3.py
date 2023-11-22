c8602da7cd531b70a4f40e58d22d82eb534e801f
EKWUEME <145931177+Ekwueme39@users.noreply.github.com>
2023-11-22 10:27:19 +0100

    Add files via upload

---
+++ app3.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from PIL import Image
+st.title("Interactive widgets")
+
+img = Image.open("cj.jpg")
+st.sidebar.image(img)
+
+st.button("Click me")
+st.checkbox("I agree")
+st.toggle("Enable")
+st.radio("Pick one", ["cats", "dogs"])
+st.selectbox("Pick one", ["cats", "dogs"])
+st.multiselect("Buy", ["milk", "apples", "potatoes"])
+st.slider("Pick a number", 0, 100)
+st.select_slider("Pick a size", ["S", "M", "L"])
+st.text_input("First name")
+st.number_input("Pick a number", 0, 10)
+st.text_area("Text to translate")
+st.date_input("Your birthday")
+st.time_input("Meeting time")
+st.file_uploader("Upload a CSV")
+st.camera_input("Take a picture")
+st.spinner("In progress")
+st.balloons()
+st.snow()
